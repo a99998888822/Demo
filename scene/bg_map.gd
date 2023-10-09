3789be39678d1f7e7f29c6d0d1b67e3e57e2a148
octopus <867187250@qq.com>
2023-10-09 23:56:40 +0800

    random map script

--- scene/bg_map.gd
+++ scene/bg_map.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var tilemap = $TileMap 
+@onready var tilemap = $TileMap
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,12 +12,14 @@ func _process(delta):
 	pass
 	
 
-func random_tile():    
-	tilemap.clear_layer(1) // 清除现有的装饰地块    
-	var bg1_cells = tilemap.get_used_cells(0)    
-	var ran = RandomNumberGenerator.new()    
-	for cell_pos in bg1_cells:        
-		var num = ran.randi_range(0, 100)        
-		if num <= 5:            
-			// 层级 起始坐标 tilemap素材的索引 索引所处图块的范围            
-			tilemap.set_cell(1, cell_pos, 0, Verctor2i(18, 1))
+func random_tile():
+	# 清除现有的装饰地块
+	tilemap.clear_layer(1)
+	var bg1_cells = tilemap.get_used_cells(0)
+	var ran = RandomNumberGenerator.new()
+	for cell_pos in bg1_cells:
+		var num = ran.randi_range(0, 100)
+		if num <= 5:
+			# 层级 起始坐标 tilemap素材的索引 索引所处图块的范围  
+			tilemap.set_cell(1, cell_pos, 1, Vector2i(18, 1))
+	pass
