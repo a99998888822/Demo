b2a2c9e11b6d67754caa61b3f1897b68c84322fa
octopusfu <octopusfu@tencent.com>
2023-10-18 17:53:15 +0800

    feat: dynamic weapon(fixing...)

--- scene/weapon/now_weapons.gd
+++ scene/weapon/now_weapons.gd
@@ -5,15 +5,27 @@ var weapon_num = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var weapon_num = self.get_child_count()
+	# 随机数生成器
+	var ran = RandomNumberGenerator.new()
+	var weapon_num = ran.randi_range(1, 6)
+	print("随机生成武器把数:" + str(weapon_num))
+	# 每把武器偏转的角度
 	var unit = TAU / weapon_num
-	var weapons = self.get_children()
+	var weapon_default = self.get_child(0, false)
+	var weapon_template = weapon_default.duplicate()
+	# 删除默认武器
+	weapon_default.free()
 	
-	for i in len(weapons):
-		var weapon = weapons[i]
+	for i in weapon_num:
+		# 这里复制的时候，weapon已经加入节点树了，所以后续_ready()方法不会再执行
+		var weapon = weapon_template.duplicate()
+		# var selected_level = 'level_' + str(ran.randi_range(1, 5))
+		# print("随机生成颜色:" + selected_level)
+
 		var weapon_rad = unit * i
 		var end_pos = weapon.position + Vector2(weapon_redius, 0).rotated(weapon_rad)
 		weapon.position = end_pos
+		self.add_child(weapon)
 	pass # Replace with function body.
 
 
--- scene/weapon/weapon.gd
+++ scene/weapon/weapon.gd
@@ -9,17 +9,15 @@ const weapon_level = {
 	level_4 = "#8847ff",
 	level_5 = "#eb4b4b",
 }
-
+		
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var ran = RandomNumberGenerator.new()
-	# var meterialTemp = weaponAni.material.duplicate()
-	# weaponAni.material = meterialTemp
-	weaponAni.material.set_shader_parameter("line_color", 
-		Color(weapon_level['level_' + str(ran.randi_range(1,5))]))
-	
+	var selected_level = 'level_' + str(ran.randi_range(1, 5))
+	print("随机生成颜色:" + selected_level)
+	weaponAni.material.set_shader_parameter("color", 
+		Color(weapon_level[selected_level]))
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
