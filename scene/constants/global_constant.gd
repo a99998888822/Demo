eef8d7d4e213969189beda435368df18c7c6def1
a99998888822 <octopusfu@tencent.com>
2023-10-30 21:07:02 +0800

    feat: gen room_box

--- scene/constants/global_constant.gd
+++ scene/constants/global_constant.gd
@@ -3,6 +3,10 @@ extends Node
 # 资源路径
 const simple_room_path = "res://scene/room_box/simple_room.tscn"
 
+# 随机地图box大小
+const room_width = 168
+const room_height = 88
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
--- scene/room_box/room_box.gd
+++ scene/room_box/room_box.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var simple_room_template = preload(GlobalConstant.simple_room_path)
-enum Direction{UP, DWON, LEFT, RIGHT}
+enum Direction{UP, DOWN, LEFT, RIGHT}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,12 +15,26 @@ func _process(delta):
 
 func init_random_room(size):
 	# 初始房间
+	var now_position = Vector2.ZERO
 	var simple_room = simple_room_template.instantiate()
+	simple_room.position = now_position
 	self.add_child(simple_room)
 	var ran = RandomNumberGenerator.new()
+	# 遍历生成房间
 	for i in range(size):
 		var dir = ran.randi_range(0, 3)
+		var room = simple_room.duplicate()
 		match dir:
 			Direction.UP:
-				print("up")
+				now_position.y += GlobalConstant.room_height
+			Direction.DOWN:
+				now_position.y -= GlobalConstant.room_height
+			Direction.LEFT:
+				now_position.x -= GlobalConstant.room_width
+			Direction.RIGHT:
+				now_position.x += GlobalConstant.room_width
+		print(dir)
+		if self.get
+		room.position = now_position
+		self.add_child(room)
 	pass
